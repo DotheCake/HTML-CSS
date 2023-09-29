b80da729ebfde8a9947f685633ba5c083a6a580a
DotheCake <marianafmollo@hotmail.com>
2023-09-28 22:12:05 -0300

    exercicios

---
+++ T.i/CSS 1/app.js
@@ -0,0 +1,8 @@
+//alert("Bem-Vindo!") // Comando de saida de informação
+/* é o comando onde vc pode fazer diversos
+comentarios em 
+diversas linhas até fechar */
+
+console.log("Bem vindo✨")
+console.log("A media é:")
+console.log((8+7+9+2)/4)
